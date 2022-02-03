139c5126243cc23d5ab1935c5efa03506e64e89d
otinomz <82697198+otinomz@users.noreply.github.com>
2022-02-03 10:58:49 +0100

    feat: more and more chnages

--- src/components/PersonalInfo.js
+++ src/components/PersonalInfo.js
@@ -2,7 +2,19 @@ import React, { Component } from "react";
 
 class PersonalInfo extends Component {
   render() {
-    return <>Personal Info</>;
+    return (
+      <>
+        <h2>Enter your personal information</h2>
+        <label>
+          <input
+            type="text"
+            name="firstName"
+            placeholder="First Name"
+            onChange={handleChange("firstName")}
+          />
+        </label>
+      </>
+    );
   }
 }
 
