6a33de5fb0e6c3d2d5bb26404bb745072d892550
otinomz <82697198+otinomz@users.noreply.github.com>
2022-02-03 10:28:48 +0100

    feat: main state chnages

---
+++ src/conponents/Main.js
@@ -0,0 +1,20 @@
+import React, { Component } from "react";
+
+class Main extends Component {
+  state = {
+    // Step 1
+    firstName: "",
+    lastName: "",
+
+    // Step 2
+    jobTitle: "",
+    jobCompany: "",
+    jobLocation: ""
+  };
+
+  render() {
+    return <>Main guy</>;
+  }
+}
+
+export default Main;
